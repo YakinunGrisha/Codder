69918d77833444cb923318e2cecb475129e8e1f2
GregoryBurton <113060763+YakinunGrisha@users.noreply.github.com>
2022-09-17 23:24:02 +0300

    Create ConsoleApp14.cs

---
+++ ConsoleApp14.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Hello World!");
+            Console.Write("");
+            a = Console.ReadLine();
+            if (a == "Hello") 
+                Console.WriteLine("Bye, world");
+            else 
+                Console.WriteLine("Bye, world");
+        }
+    }
+}
